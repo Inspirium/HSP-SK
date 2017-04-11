4ecec32a8756f1833df38635ebc8e6c6a10dd1ae
Marko Banusic <mbanusic@gmail.com>
2017-04-11 14:18:12 +0200

    Still initial

--- config/app.php
+++ config/app.php
@@ -177,6 +177,11 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+	    Inspirium\UserManagement\UserManagementServiceProvider::class,
+	  //  Inspirium\HumanResources\HumanResourcesServiceProvider::class,
+        Inspirium\SKTemplate\SKTemplateServiceProvider::class,
+
+	    JeroenG\Packager\PackagerServiceProvider::class,
     ],
 
     /*
