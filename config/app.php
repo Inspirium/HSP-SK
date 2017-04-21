fd67241f3ca609757760cfae946c0b985b97cce5
Marko Banusic <mbanusic@gmail.com>
2017-04-21 12:42:09 +0200

    Initial proposition setup

--- config/app.php
+++ config/app.php
@@ -188,6 +188,8 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        Inspirium\BookProposition\BookPropositionServiceProvider::class,
+
  //       Inspirium\FormBuilder\FormBuilderServiceProvider::class,
 
 	    Inspirium\UserManagement\UserManagementServiceProvider::class,
