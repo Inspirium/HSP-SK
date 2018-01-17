3de526993bf00652405fa590bcb49e8da01edf97
Marko Banusic <mbanusic@gmail.com>
2018-01-17 16:37:51 +0100

    Fix to remove deleted propositions

--- app/Http/Controllers/Api/HomeController.php
+++ app/Http/Controllers/Api/HomeController.php
@@ -1,21 +1,22 @@
 <?php
 namespace Inspirium\Http\Controllers\Api;
 
+use Inspirium\BookProposition\Models\BookProposition;
 use Inspirium\Http\Controllers\Controller;
+use Inspirium\Models\FileManagement\File;
 
 class HomeController extends Controller {
 	public function home() {
-		$total = \DB::table('propositions')->count();
-		$approval = \DB::table('propositions')->where('status', 'requested')->count();
-		$files = \DB::table('fileables')->where('fileable_type', 'Inspirium\BookProposition\Models\BookProposition')->get(['fileable_id', 'type']);
+		$total = BookProposition::count();
+		$approval = BookProposition::where('status', 'requested')->count();
+		$files = \DB::table('fileables')->where('fileable_type', 'Inspirium\BookProposition\Models\BookProposition')->join('propositions', 'id', 'fileable_id')->whereNull('propositions.deleted_at')->get(['fileable_id', 'type']);
 		$latest = [];
 		$order = [
 			'translation', 'technical_preparation', 'proofreading', 'additional_materials', 'reviews', 'lecture', 'technical_correction', 'final_review',
 			'cover_design', 'layout_design',
 			'first_block_layout', 'cover', 'layout', 'first_revision', 'correction', 'correction_input', 'revisions',
 			'print_proof', 'print_proof_correction',
-			'print',
-
+			'print'
 		];
 		foreach ($files as $file) {
 			if (isset($latest[$file->fileable_id])) {
