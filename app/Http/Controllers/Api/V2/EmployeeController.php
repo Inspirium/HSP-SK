565e5cbfe9b355c8be1aa441ca346d9b97f8e6d5
Marko Banušić <mbanusic@gmail.com>
2022-02-26 16:25:52 +0100

    save base64 image on employee update

--- app/Http/Controllers/Api/V2/EmployeeController.php
+++ app/Http/Controllers/Api/V2/EmployeeController.php
@@ -2,7 +2,12 @@
 
 namespace Inspirium\Http\Controllers\Api\V2;
 
+use Inspirium\Actions\SaveImageAction;
 use Inspirium\Http\Controllers\Controller;
+use Inspirium\JsonApi\V2\Employees\EmployeeQuery;
+use Inspirium\JsonApi\V2\Employees\EmployeeRequest;
+use Inspirium\JsonApi\V2\Employees\EmployeeSchema;
+use Inspirium\Models\HumanResources\Employee;
 use LaravelJsonApi\Core\Responses\DataResponse;
 use LaravelJsonApi\Laravel\Http\Controllers\Actions;
 
@@ -22,5 +27,14 @@ class EmployeeController extends Controller
 
     public function me() {
         return DataResponse::make(\Auth::user());
+    }
+
+    public function saved(Employee $employee, EmployeeRequest $request, EmployeeQuery $query) {
+        $image = $request->data['attributes']['image'];
+        if ($image && str_contains($image, 'data')) {
+            $saveImageAction = new SaveImageAction();
+            $employee->image = $saveImageAction->execute($image, 'avatars');
+            $employee->save();
+        }
     }
 }
