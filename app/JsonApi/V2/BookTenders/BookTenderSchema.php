3c5a9e3c32d5cdf9dfc6cfb6ccd35864ccfc19fd
GlitchMode <dominic.galic@gmail.com>
2022-03-16 18:40:50 +0100

    BookTender

--- app/JsonApi/V2/BookTenders/BookTenderSchema.php
+++ app/JsonApi/V2/BookTenders/BookTenderSchema.php
@@ -6,6 +6,7 @@ use Inspirium\Models\BookManagement\BookTender;
 use LaravelJsonApi\Eloquent\Contracts\Paginator;
 use LaravelJsonApi\Eloquent\Fields\DateTime;
 use LaravelJsonApi\Eloquent\Fields\ID;
+use LaravelJsonApi\Eloquent\Fields\Relations\HasMany;
 use LaravelJsonApi\Eloquent\Filters\WhereIdIn;
 use LaravelJsonApi\Eloquent\Pagination\PagePagination;
 use LaravelJsonApi\Eloquent\Schema;
@@ -29,6 +30,8 @@ class BookTenderSchema extends Schema
     {
         return [
             ID::make(),
+            HasMany::make('books'),
+            HasMany::make('propositions'),
             DateTime::make('createdAt')->sortable()->readOnly(),
             DateTime::make('updatedAt')->sortable()->readOnly(),
         ];
